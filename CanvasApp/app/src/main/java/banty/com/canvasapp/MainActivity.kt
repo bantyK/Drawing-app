b9a633007d06441d7aed3f13dfd6b0d26b797937
bantyK <bantyp92@gmail.com>
2018-11-26 10:13:07 +0530

    save in gallary functionality added

--- CanvasApp/app/src/main/java/banty/com/canvasapp/MainActivity.kt
+++ CanvasApp/app/src/main/java/banty/com/canvasapp/MainActivity.kt
@@ -1,12 +1,20 @@
 package banty.com.canvasapp
 
 import android.app.AlertDialog
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -44,8 +52,83 @@ class MainActivity : AppCompatActivity() {
             R.id.action_redo -> {
                 drawingView.redoDrawing()
             }
+
+            R.id.action_save -> {
+                showSaveDrawingDialog()
+            }
         }
         return false
+    }
+
+    fun showSaveDrawingDialog() {
+        //save drawing attach to Notification Bar and let User Open Image to share.
+        val saveDialog = AlertDialog.Builder(this)
+        saveDialog.setTitle("Save drawing")
+        saveDialog.setMessage("Save drawing to device Gallery?")
+        saveDialog.setPositiveButton(
+            "Yes"
+        ) { dialog, which -> saveDrawing() }
+        saveDialog.setNegativeButton(
+            "Cancel"
+        ) { dialog, which -> dialog.cancel() }
+        saveDialog.show()
+    }
+
+    private fun saveDrawing() {
+
+    }
+
+    fun saveThisDrawing() {
+        var path = Environment.getExternalStorageDirectory().toString()
+        path = path + "/" + getString(R.string.app_name)
+        val dir = File(path)
+        //save drawing
+        drawingView.setDrawingCacheEnabled(true)
+
+        //attempt to save
+        val imTitle = "Drawing" + "_" + System.currentTimeMillis() + ".png"
+        val imgSaved = MediaStore.Images.Media.insertImage(
+            contentResolver, drawingView.getDrawingCache(),
+            imTitle, "a drawing"
+        )
+
+        try {
+            if (!dir.isDirectory || !dir.exists()) {
+                dir.mkdirs()
+            }
+            drawingView.isDrawingCacheEnabled = true
+            val file = File(dir, imTitle)
+            val fOut = FileOutputStream(file)
+            val bm = drawingView.getDrawingCache()
+            bm.compress(Bitmap.CompressFormat.PNG, 100, fOut)
+
+
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("Uh Oh!")
+            alert.setMessage("Oops! Image could not be saved. Do you have enough space in your device?1")
+            alert.setPositiveButton("OK", null)
+            alert.show()
+
+        } catch (e: IOException) {
+            val unsavedToast = Toast.makeText(
+                applicationContext,
+                "Oops! Image could not be saved. Do you have enough space in your device2?", Toast.LENGTH_SHORT
+            )
+            unsavedToast.show()
+            e.printStackTrace()
+        }
+
+        if (imgSaved != null) {
+            val savedToast = Toast.makeText(
+                applicationContext,
+                "Drawing saved to Gallery!", Toast.LENGTH_SHORT
+            )
+            savedToast.show()
+        }
+
+        drawingView.destroyDrawingCache()
     }
 
 
